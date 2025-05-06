fa038c5db24e79ef7a1d1ca1c341b287899644b4
Cauz <urcauzz@gmail.com>
2025-05-06 15:03:34 +0530

    Removed reportWebVitals call

--- backend/models/Note.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  summary: String,
-  owner: String
-}, { timestamps: true });
-
-export default mongoose.model("Note", noteSchema);
